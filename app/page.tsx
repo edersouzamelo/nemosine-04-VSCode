3d756f03b78289f6389cf8a7cfa4ca4019ec7308
Edervaldo José de Souza Melo <edersouzamelo@gmail.com>
2026-02-16 15:44:37 -0400

    fix: connect login buttons to agents dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import MedievalButton from "./components/MedievalButton";
 
 export default function Home() {
@@ -29,19 +30,23 @@ export default function Home() {
         {/* Login Box */}
         <div className="glass-medieval p-8 w-full flex flex-col gap-6 items-center">
           <div className="w-full space-y-4">
-            <MedievalButton className="w-full">
-              <svg className="w-5 h-5" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
-                <path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-1 .67-2.26 1.07-3.71 1.07-2.87 0-5.3-1.94-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
-                <path fill="currentColor" d="M5.84 14.09c-.22-.67-.35-1.39-.35-2.14s.13-1.47.35-2.14V7.01H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.99l3.66-2.9z" />
-                <path fill="currentColor" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.01l3.66 2.91c.86-2.59 3.3-4.54 6.16-4.54z" />
-              </svg>
-              Entrar com Google
-            </MedievalButton>
+            <Link href="/agents" className="block w-full">
+              <MedievalButton className="w-full">
+                <svg className="w-5 h-5" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
+                  <path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-1 .67-2.26 1.07-3.71 1.07-2.87 0-5.3-1.94-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
+                  <path fill="currentColor" d="M5.84 14.09c-.22-.67-.35-1.39-.35-2.14s.13-1.47.35-2.14V7.01H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.99l3.66-2.9z" />
+                  <path fill="currentColor" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.01l3.66 2.91c.86-2.59 3.3-4.54 6.16-4.54z" />
+                </svg>
+                Entrar com Google
+              </MedievalButton>
+            </Link>
 
-            <MedievalButton variant="secondary" className="w-full">
-              Outras Contas
-            </MedievalButton>
+            <Link href="/agents" className="block w-full">
+              <MedievalButton variant="secondary" className="w-full">
+                Outras Contas
+              </MedievalButton>
+            </Link>
           </div>
 
           <div className="w-full flex items-center gap-3">
